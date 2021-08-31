07d4a215a65a8aba38c25e3b6ab7211365b7cff7
Isabell Lee <izzy.isabell@gmail.com>
2021-08-31 15:58:57 -0700

    Implemented form, can submit text

--- csv-report-generator/server.js
+++ csv-report-generator/server.js
@@ -1,5 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('client'));
+//app.use(express.json());
+app.use(bodyParser.text());
+
+
+app.get('/', (req, res) => {
+  console.log(req.body);
+
+  res.status(200).send('hello');
+})
+
+app.post('/upload_json', (req, res) => {
+  var reqBody = req.body;
+  var upload = reqBody.slice(7);
+
+  console.log(upload);
+
+  var uploadObj = JSON.parse(upload);
+
+
+
+
+
+  res.end('posted');
+
+});
+
+
+app.listen(3000, () => console.log("Listending to server"));
+
